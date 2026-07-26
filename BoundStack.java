ef215d12cc0a2647f90ff6c142add199f5553eb0
banthita-tho <banthita.tho@ku.th>
2026-07-26 11:31:53 +0700

    Add files via upload

---
+++ BoundStack.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+/**
+ * Boundstack คือ
+ */
+public class BoundStack {
+
+    private final List<String> elements ;
+    private final int capacity ;
+
+    //AF(elements,capacity) =
+    //RI
+    // -
+    // -
+
+}
+
+/**
+ * 
+ * @param capacity
+ */
+public BoundStack(int capacity){
+    this.elements = new ArrayList<>();
+    this.capacity = capacity ;
+}
+
+/**
+ * 
+ * @param s
+ */
+public void push(String s){
+    
+}
